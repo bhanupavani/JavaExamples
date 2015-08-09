41a8b5b518a9a0e3b7521100c53e6af64d88ee9d
bhanupavani <bhanupavani@gmail.com>
2015-08-10 00:08:08 +0530

    Added StringTokenizer logic

--- src/com/java/examples/io/FileReading.java
+++ src/com/java/examples/io/FileReading.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class FileReading {
 
@@ -15,15 +16,25 @@ public class FileReading {
 			String line = null;
 
 			while ((line = bufferedReader.readLine()) != null) {
+				StringTokenizer tokenizer = new StringTokenizer(line, "|");
+				while(tokenizer.hasMoreTokens()){
+					String element = (String)tokenizer.nextElement();
+					System.out.println(element);
+				}
 				System.out.println(line);
 			}
 		} catch (FileNotFoundException e) {
+			
 			System.out.println("File not found in the given location");
 		} catch (IOException e) {
 			System.out.println("Error while reading the file");
 		} finally {
-			bufferedReader.close();
+			if (bufferedReader != null) {
+				bufferedReader.close();
+			}
 		}
+
+		System.out.println("File reading completed.");
 	}
 
 }
